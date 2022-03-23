a922d1a1e8d3635fe3c271ac3337cb3f8d1320d6
Vladislav Russo <vladtirma@gmail.com>
2022-03-24 02:41:06 +0300

    L`

---
+++ sorting_by_choice.py
@@ -0,0 +1,14 @@
+def find_Smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(i,len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+        return smallest_index
+def slsectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = find_Smallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
